59371fbf80e46837f5516637c317c75f8e807476
Egor <103055557+Egorkka74@users.noreply.github.com>
2022-04-07 21:00:00 +0300

    School 21
    School 21

---
+++ cleaner.sh
@@ -0,0 +1,17 @@
+rm -rf ~/Library/Caches/
+rm -rf ~/Library/*42_cache*
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/
+rm -rf ~/Library/Application\ Support/Slack/Cache/
+rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/
+rm -rf ~/Library/Application\ Support/Code/Cache
+rm -rf ~/Library/Application\ Support/Code/CachedData
+rm -rf ~/Library/Application\ Support/Code/CachedExtension
+rm -rf ~/Library/Application\ Support/Code/CachedExtensions
+rm -rf ~/Library/Application\ Support/Code/CachedExtensionVSIXs
+rm -rf ~/Library/Application\ Support/Code/Code\ Cache
+rm -rf ~/Library/Application\ Support/Spotify/PersistentCache
+rm -rf ~/Library/Application\ Support/discord/Cache/
+rm -rf ~/Library/Safari/
+rm -rf ~/Library/Cookies/
+rm -rf ~/Library/Application Support/Spotify/PersistentCache
+rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage
